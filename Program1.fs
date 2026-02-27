fabeffd3b99d1bf199802c6771b851e77381e704
societyladen <denis.society@ya.ru>
2026-02-27 10:41:49 +0500

    Program1.fs

--- Program1.fs
+++ Program1.fs
@@ -1,14 +1,16 @@
-﻿open System
+open System
 
 // Задание 1
-let lengthsLine () =
-    [ for _ in 1 .. 5 do
+let lengthsLine count =
+    [ for _ in 1 .. count do
         printf "Введите строку: "
         let line = Console.ReadLine()
         yield line.Length ]
 
 [<EntryPoint>]
 let main args =
-    let lengths = lengthsLine ()
+    printf "Введите количество строк: "
+    let count = Console.ReadLine() |> int
+    let lengths = lengthsLine count
     printfn "Длины строк: %A" lengths
     0
